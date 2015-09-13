44f338bbef7b7a518c0649de0cae91593b788c8a
BrayanRosas <brayan_rosas@hotmail.com>
2015-09-13 11:18:32 -0400

    adding the method ._saveScore in class player

--- app/js/player.js
+++ app/js/player.js
@@ -1,6 +1,12 @@
 var Player = function(name){
     this.name = name;
     this.score = 0;
-    this.table = new Table(8);
     this.shots = 0;
 };
+
+Player.prototype._setTable = function(size){
+   this.table=new Table(size);
+};
+
+
+
