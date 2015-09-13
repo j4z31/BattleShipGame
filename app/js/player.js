01231515d2c70705696705792b74fcf12254cbfb
BrayanRosas <brayan_rosas@hotmail.com>
2015-09-13 14:57:17 -0400

    Player with store score

--- app/js/player.js
+++ app/js/player.js
@@ -4,9 +4,30 @@ var Player = function(name){
     this.shots = 0;
 };
 
-Player.prototype._setTable = function(size){
-   this.table=new Table(size);
+
+Player.prototype._saveScore = function(timeUsed,shootUsed){
+
+
+    this.score = timeUsed*100-shootUsed*10;
+    var scorePlayer=this.score;
+    var playerName="";
+    playerName=this.name;
+    var playerScore = { playerName:playerName,'Score':scorePlayer };
+
+// Put the object into storage
+    localStorage.setItem('playerScore', JSON.stringify(playerScore));
+    this._showScore('playerScore');
+// Retrieve the object from storage
 };
 
+Player.prototype._showScore=function(playerScore)
+{
+    var scoreGet = localStorage.getItem(playerScore);
+
+    console.log( JSON.parse( localStorage.getItem( playerScore ) ) );
+    console.log('The player :', JSON.parse(scoreGet).playerName,'have',JSON.parse(scoreGet).Score,
+        'Points of Score');
+
+};
 
 
