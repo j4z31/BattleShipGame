bbcc99784cadec7b0f0b952498e88280490fc5bf
AlejandroJeanCarlo <alejandrojeancarlo@gmail.com>
2015-09-12 20:39:47 -0400

    Update table.js

--- app/js/table.js
+++ app/js/table.js
@@ -1,38 +1,73 @@
+/*var Table = new function(size){
+  this.size = size;
+  this.direction = false;
+  //this.id =
+  this.ships = [];
+  this._createShips();
+};
+
+Game.prototype.start = function(){
+  var _ship = new Ship(pos, tam, dir);
+  this.ships.push(_ship);
+};*/
+
+//Trainner
+
 var Table = function(size){
-    this.size = size;
-    this.ships = [];
-    this._field = [];
-    this._createShips();
-    this._initField();
-    this._placeShips();
-
+  this.size = size;
+  this.ships = [];
+  this._field = [];
+  this._initField();
+  this._createShips();
+  this._placeShips();
 };
 
-Table.prototype._createShips = function ()
-{
-    // TODO: Number of ships should be read from a constant
-    for (var i = 0; i < numShips; i++)
-    {
-        var ship = new Ship(i,3);
-        this.ships.push(ship);
+Table.prototype._initField = function(){
+  for (var i = 0; i < this.size; i++) {
+    this._field.push('-');
+  }
+};
+
+Table.prototype._createShips = function(){
+  //TODO: Number of shipe should be retrieved from a constant
+  var numShips = 2;
+  var ship;
+  for (var i = 0; i < numShips; i++) {
+    ship = new Ship(i, 3);
+    this.ships.push(ship);
+  }
+};
+
+Table.prototype._placeShips = function(){
+  for (var i = 0; i < this.ships.length; i++) {
+    var ship = this.ships[i];
+    var initPos = parseInt(Math.random() * (this.size - ship.size));
+    for (var j = initPos; j <(initPos+ship.size); j++) {
+      this._field[j] = ship.id;
     }
+  }
 };
 
-Table.prototype._initField = function ()
-{
-    for (var i = 0; i < this.size; i++)
-    {
-        this._field.push('-');
+
+
+  /*Table.prototype._placeShips = function(){
+    var pos = parseInt(Math.random() * this.size);
+
+    for (var i = 0; i < this.size; i++) {
+      for (var i = 0; i < this.ships.length; i++) {
+        if (pos + ships[i]._getTam() < this.size &&
+          notColision(pos, ships[i]) ) {
+            drawShip(ships[i]);
+        }
+      }
     }
-}
+  };
 
-Table.prototype._placeShips = function()
-{
-    var initPosition;
-    initPosition = parseInt(Math.random()*(this.size-this.ships[0].getSize()));
-    for (var i = 0; i < this.ships[0].getSize() ; i ++ )
-    {
-        this._field[initPosition+i] = this.ships[0].getID();
-    };
-};
-
+  Table.prototype._notColision = function(pos, ship){
+    var res = false;
+    for (var i = 0; i < field.length; i++) {
+      if (field[i] !== '-') {
+        res = true
+      }
+    }
+  };*/
