cc311b76da07f09c28fccd933af0f2045d709047
j4z31 <tumael_777@hotmail.com>
2015-09-13 22:40:53 -0400

    :

--- app/js/game.js
+++ app/js/game.js
@@ -2,35 +2,77 @@ var Game = function(name, tam){
   this.name = name;
   // TODO: Number of shots should be retrieved from a constant
   var maxNumShots = 10;
-  this.getMaxNumShots = function(){return maxNumShots};
+  getMaxNumShots = function(){return maxNumShots};
 
   var players = [];
-  this.getPlayers = function(){return players};
+  getPlayers = function(){return players};
   
   var table;
-  this.getTable = function(){return table};
+  setTable = function(){
+    var tamTable = window.prompt('Size of your table');
+    table = new Table(tamTable);
+  };
+
+  getTable = function(){
+    return table;
+  };
 };
 
 Game.prototype.start = function(){
   console.log(this.name, ' Started!!!');
-  this._setGame();
+  this._setGame();  
   
   // DISPARO
   var numShots = 0;
   do{
-    console.log('TMP: Table', this.getTable().display());    
+    getTable().display();
     numShots++;
 
     //EVALUAR EL DISPARO
     
-  }while(numShots < this.getMaxNumShots());
+  }while(numShots < getMaxNumShots());
 };
 
 Game.prototype._setGame = function(){
   //TODO: Number of players should bn retrieved from a constant
   var name = window.prompt('What is your name');
-  var tamTable = window.prompt('Size of your table');
-  this.players.push(new Player(name));
-  var table = this.getTable();
-  table = new Table (tamTable);
+  //var tamTable = window.prompt('Size of your table');
+  getPlayers().push(new Player(name));
+  setTable();
+  this.createShips();
+};
+
+//Type1 - Cruise, Type2 - Submarine and Type3 - Aircraft Carrier
+Game.prototype.createShips = function(){
+  var ships = [];
+  var id = 0;
+
+  //Metodo aumentado en table.size()
+  //Table.prototype.getSize = function(){
+    //return getSize() * getSize();
+  //};
+  var tam = getTable().getSize() / 2;
+
+  // Cruise
+  for (var i = 0; i < Math.round((tam *0.31)/2); i++) {
+    ship = new Ship(id, 2);
+    ships.push(ship);
+    id ++;    
+  };  
+
+  //Submarine
+  for (var i = 0; i < Math.round((tam *0.37)/3); i++) {
+    ship = new Ship(id, 3);
+    ships.push(ship);
+    id ++;
+  };
+
+  //Aircraft Carrier
+  for (var i = 0; i < Math.round((tam *0.31)/5); i++) {
+    ship = new Ship(id, 5);
+    ships.push(ship);
+    id ++;
+  };
+
+  return ships;
 };
